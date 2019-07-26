5cfeeb31a2814a263d1eb6481ac84a999d2a6355
coti_garcia <javiera.garcialatorre@gmail.com>
2019-07-26 17:44:56 -0400

    Question added to DOM from API

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
     
+    
+    
+    
+    
     $.ajax({
         url: "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple",
         method: "GET"
     }).then(function(response){
-        console.log(response);
-        
+        console.log(response.results)
+        let qContainer = $("<div class='card'>");
+        let qTitle = $('<div class="card-header">');
+        let qAnswerContainer = $('<ul class="list-group list-group-flush">')
+        let qAnswer = $('<li class="list-group-item">')
+        function addQuestion(){
+            qAnswerContainer.append(qAnswer);
+            qContainer.append(qTitle,qAnswerContainer);
+            $(".question-container").html(qContainer);
+        }
+        addQuestion();
+        qTitle.text(response.results[0].question)
     });
 
 }); //Close line
