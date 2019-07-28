147676a8d8a8ad22d9d28dec47682fbecd03fc5e
coti_garcia <javiera.garcialatorre@gmail.com>
2019-07-28 13:01:58 -0400

    event click working ok

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,20 +25,28 @@ $(document).ready(function(){
         const qContainer = $("<div class='card'>");
         const qTitle = $('<div class="card-header">');
         const qAnswerContainer = $('<ul class="list-group list-group-flush">')
-
+        const qAnswer = $('<li class="list-group-item">')
         qContainer.append(qTitle,qAnswerContainer);
         $(".question-container").html(qContainer);
         qTitle.text(textTitle)
         // loop to add answers to the DOM:
+        // ASK TO JACOB:  Why all the buttons are in one <li>??
         for(var i=0; i<allAnswers.length ; i++){
-                var ansOption = $(`<li class="list-group-item">${allAnswers[i]}</li>`);
-                ansOption.attr("data-answer", allAnswers[i]);
-                $(qAnswerContainer).append(ansOption);
+                var ansOption = $(`<button class="btn-ans" value="${allAnswers[i]}">${allAnswers[i]}</li>`);
+                $(qAnswer).append(ansOption);
+                $(qAnswerContainer).append(qAnswer);
         }
-              
-        // qAnswer.text(textCorrectAns);
-
         
+        $(".btn-ans").click(function(){
+            let value = $(this).attr("value");
+            //console.log(value);
+            if ( value == textCorrectAns){
+                 console.log("win!")
+            }else{
+                console.log("loose!")
+            }
+        });
+
         
     });
 
