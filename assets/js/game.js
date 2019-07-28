c63a8bbbf22c0ce8d0173ad5a17ffbed4c6ec414
coti_garcia <javiera.garcialatorre@gmail.com>
2019-07-28 15:47:14 -0400

    Try to reset the game

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,54 +7,70 @@ $(document).ready(function(){
         method: "GET"
     }).then(function(response){
         let qIndex = 0;
-        // Text Variables
         const textTitle = response.results[qIndex].question;
         const textCorrectAns = response.results[qIndex].correct_answer;
         const incorrectAns = response.results[qIndex].incorrect_answers; //Array 3
         incorrectAns.push(textCorrectAns);
         const allAnswers = incorrectAns;
-        
-        console.log("all: " + allAnswers)
-        console.log(textTitle);
-        console.log("incorrect:" + incorrectAns);
-        console.log("correct:" + textCorrectAns);
-        
+
         // DOM Manipulation
-        const qContainer = $("<div class='card'>");
-        const qTitle = $('<div class="card-header">');
-        const qAnswerContainer = $('<ul class="list-group list-group-flush">')
-        const qAnswer = $('<li class="list-group-item">')
-        qContainer.append(qTitle,qAnswerContainer);
-        $(".question-container").html(qContainer);
-        qTitle.html(textTitle)
-            // loop to add answers to the DOM:
-            // ASK TO JACOB:  Why all the buttons are in one <li>??
-        for(var i=0; i<allAnswers.length ; i++){
-                var ansOption = $(`<button class="btn-ans" value="${allAnswers[i]}">${allAnswers[i]}</li>`);
-                $(qAnswer).append(ansOption);
-                $(qAnswerContainer).append(qAnswer);
+        function game(){
+            const qContainer = $("<div class='card'>");
+            const qTitle = $('<div class="card-header">');
+            const qAnswerContainer = $('<ul class="list-group list-group-flush">')
+            const qAnswer = $('<li class="list-group-item">')
+            qContainer.append(qTitle,qAnswerContainer);
+            $(".question-container").html(qContainer);
+            qTitle.html(textTitle)
+                // loop to add answers to the DOM:
+                // ASK TO JACOB:  Why all the buttons are in one <li>??
+            for(var i=0; i<allAnswers.length ; i++){
+                    var ansOption = $(`<button class="btn-ans" value="${allAnswers[i]}">${allAnswers[i]}</li>`);
+                    $(qAnswer).append(ansOption);
+                    $(qAnswerContainer).append(qAnswer);
+            }
         }
+        game();
+        
+        function newGame(){
+            $(".question-container").empty()
+            qIndex ++;
+            game();
+            console.log(qIndex);
+        }   
         
         // Event Listener Win/Loose
         $(".btn-ans").click(function(){
             let value = $(this).attr("value");
             if ( value == textCorrectAns){
-                 console.log("win!")
+                console.log("win!")
+                $("#message-display").append("<h3>You Won!</h3");
+                $("#message-display").append('<button id="next">next</button>');
+                $("#next").click(function(){
+                    newGame();
+                })
+
             }else{
                 console.log("loose!")
+                $("#message-display").append("<h3>You lost. Try another one!</h3");
+                $("#message-display").append('<button id="next">next</button>');
+                $("#next").click(function(){
+                    newGame();
+                })
             }
         });
+
+
+       
 
 
 
         //Clock
         let count = 30;
         $("#seconds").html(count);
-
         function countDown(){
             count--;
             $("#seconds").html(count);
-            console.log("hi!");
         }
         setTimeout(countDown, 1000);
 
