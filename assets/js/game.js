94f2d8aa2101a922a4b40b334d7f345e89c2136e
coti_garcia <javiera.garcialatorre@gmail.com>
2019-07-28 12:25:41 -0400

    all question in one array

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,6 +13,10 @@ $(document).ready(function(){
         const textTitle = response.results[qIndex].question;
         const textCorrectAns = response.results[qIndex].correct_answer;
         const incorrectAns = response.results[qIndex].incorrect_answers; //Array 3
+        incorrectAns.push(textCorrectAns);
+        const allAnswers = incorrectAns;
+        
+        console.log("all: " + allAnswers)
         console.log(textTitle);
         console.log("incorrect:" + incorrectAns);
         console.log("correct:" + textCorrectAns);
@@ -21,20 +25,19 @@ $(document).ready(function(){
         const qContainer = $("<div class='card'>");
         const qTitle = $('<div class="card-header">');
         const qAnswerContainer = $('<ul class="list-group list-group-flush">')
-        const qAnswer = $('<li class="list-group-item">')
 
         qContainer.append(qTitle,qAnswerContainer);
         $(".question-container").html(qContainer);
-
-        // loop to add the 3 incorrect answers:
-        for(var i=0; i<incorrectAns.length ; i++){
-                var ansOption = $(`<li class="list-group-item">${incorrectAns[i]}</li>`);
+        qTitle.text(textTitle)
+        // loop to add answers to the DOM:
+        for(var i=0; i<allAnswers.length ; i++){
+                var ansOption = $(`<li class="list-group-item">${allAnswers[i]}</li>`);
+                ansOption.attr("data-answer", allAnswers[i]);
                 $(qAnswerContainer).append(ansOption);
         }
               
-        qTitle.text(textTitle)
-        qAnswer.text(textCorrectAns);
-        qAnswerContainer.append(qAnswer);
+        // qAnswer.text(textCorrectAns);
+
         
         
     });
