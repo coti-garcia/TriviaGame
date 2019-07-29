c2c39934f7acb39714469e4b83ba57056eaf58a5
coti_garcia <javiera.garcialatorre@gmail.com>
2019-07-29 17:38:18 -0400

    Count 00 start new game

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,21 +3,52 @@ $(document).ready(function(){
         url: "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple",
         method: "GET"
     }).then(function(response){ 
+        
         // QUESTION API INFORMATION
         let qIndex = 0;
             let textTitle = response.results[qIndex].question;
             let textCorrectAns = response.results[qIndex].correct_answer;
-            let incorrectAns = response.results[qIndex].incorrect_answers; //Array 3
-                  incorrectAns.push(textCorrectAns);
-                  incorrectAns.sort();
+            let incorrectAns = response.results[qIndex].incorrect_answers; //Array 3 
             let allAnswers = incorrectAns; //Array 4
         
-        console.log(textCorrectAns);       
         // DOM Manipulation
         const qContainer = $("<div class='card'>");
         const qTitle = $('<div class="card-header">');
         const qAnswerContainer = $('<ul class="list-group list-group-flush">')
         
+        
+        $("#start-game").html("<button id='begin'>Start Game</button>");
+        
+        //Clock
+        let count = 00;
+        function countDown(){
+            if(count > 0){
+                count--;
+                $("#seconds").html(count);
+                setTimeout(countDown, 1000);
+            }else{
+                $("#message-display").append("<h3>You lost. Try another one!</h3")
+                setTimeout(emptyContent, 4000);
+                qIndex++;
+                setTimeout(nextQuestion,4001);
+                console.log(qIndex);
+            }
+        }
+        
+        // Add Start Button to DOM
+       
+
+        $("#begin").click(function(){
+            nextQuestion();
+        })
+        
+        
+        function emptyContent(){
+            $(".question-container").empty();
+            $("#message-display").empty();
+            $(qAnswerContainer).empty();
+        }
+
         function displayGameContainers(){
             qContainer.append(qTitle,qAnswerContainer);
             $(".question-container").html(qContainer);
@@ -27,9 +58,10 @@ $(document).ready(function(){
             textTitle = response.results[qIndex].question;
             textCorrectAns = response.results[qIndex].correct_answer;
             incorrectAns = response.results[qIndex].incorrect_answers; //Array 3
-            // incorrectAns.push(textCorrectAns);
-            // incorrectAns.sort();
+            incorrectAns.push(textCorrectAns);
+            incorrectAns.sort();
             allAnswers = incorrectAns; //Array 4
+            console.log(textCorrectAns);       
             qTitle.html(textTitle)
             for(var i=0; i<allAnswers.length ; i++){
                     var ansOption = $(`<li class="list-group-item"><button class="btn-ans" value="${allAnswers[i]}">${allAnswers[i]}</button></li>`);
@@ -38,15 +70,14 @@ $(document).ready(function(){
             }
         }
             
-        displayGameContainers();
-        textGame(0);
-        
+       
         function nextQuestion(){
-            $(".question-container").empty()
-            qIndex ++;
-            textGame();
+            count = 20;
+            $("#seconds").html(count);
+            $("#start-game").empty();
             displayGameContainers();
-            console.log(qIndex);
+            textGame();
+            setTimeout(countDown, 1000);
         }   
         
         // Event Listener Win/Loose
@@ -70,18 +101,7 @@ $(document).ready(function(){
 
 
 
-        let count = 10;
-        function countDown(){
-            let timer = $("#seconds");
-            if(count > 0){
-                count--;
-                timer.html(count);
-                setTimeout(countDown, 1000);
-            }else{
-                nextQuestion();
-            }
-        }
-        countDown();
+        
 
 
 
