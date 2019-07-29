9572852ed2714ec3573041334a8166ca9ed1ba99
coti_garcia <javiera.garcialatorre@gmail.com>
2019-07-29 16:57:10 -0400

    clock working ok and added target to footer link

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,82 +1,92 @@
 $(document).ready(function(){
-    
-    
-    
     $.ajax({
         url: "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple",
         method: "GET"
-    }).then(function(response){
+    }).then(function(response){ 
+        // QUESTION API INFORMATION
         let qIndex = 0;
-        const textTitle = response.results[qIndex].question;
-        const textCorrectAns = response.results[qIndex].correct_answer;
-        const incorrectAns = response.results[qIndex].incorrect_answers; //Array 3
-        incorrectAns.push(textCorrectAns);
-        const allAnswers = incorrectAns;
-
+            let textTitle = response.results[qIndex].question;
+            let textCorrectAns = response.results[qIndex].correct_answer;
+            let incorrectAns = response.results[qIndex].incorrect_answers; //Array 3
+                  incorrectAns.push(textCorrectAns);
+                  incorrectAns.sort();
+            let allAnswers = incorrectAns; //Array 4
+        
+        console.log(textCorrectAns);       
         // DOM Manipulation
-        function game(){
-            const qContainer = $("<div class='card'>");
-            const qTitle = $('<div class="card-header">');
-            const qAnswerContainer = $('<ul class="list-group list-group-flush">')
-            const qAnswer = $('<li class="list-group-item">')
+        const qContainer = $("<div class='card'>");
+        const qTitle = $('<div class="card-header">');
+        const qAnswerContainer = $('<ul class="list-group list-group-flush">')
+        
+        function displayGameContainers(){
             qContainer.append(qTitle,qAnswerContainer);
             $(".question-container").html(qContainer);
+        }    
+        
+        function textGame(){
+            textTitle = response.results[qIndex].question;
+            textCorrectAns = response.results[qIndex].correct_answer;
+            incorrectAns = response.results[qIndex].incorrect_answers; //Array 3
+            // incorrectAns.push(textCorrectAns);
+            // incorrectAns.sort();
+            allAnswers = incorrectAns; //Array 4
             qTitle.html(textTitle)
-                // loop to add answers to the DOM:
-                // ASK TO JACOB:  Why all the buttons are in one <li>??
             for(var i=0; i<allAnswers.length ; i++){
-                    var ansOption = $(`<button class="btn-ans" value="${allAnswers[i]}">${allAnswers[i]}</li>`);
-                    $(qAnswer).append(ansOption);
-                    $(qAnswerContainer).append(qAnswer);
+                    var ansOption = $(`<li class="list-group-item"><button class="btn-ans" value="${allAnswers[i]}">${allAnswers[i]}</button></li>`);
+                    $(qAnswerContainer).append(ansOption);
+                    //$(qAnswerContainer).append(qAnswer);
             }
         }
-        game();
+            
+        displayGameContainers();
+        textGame(0);
         
-        function newGame(){
+        function nextQuestion(){
             $(".question-container").empty()
             qIndex ++;
-            game();
+            textGame();
+            displayGameContainers();
             console.log(qIndex);
         }   
         
         // Event Listener Win/Loose
         $(".btn-ans").click(function(){
             let value = $(this).attr("value");
-            if ( value == textCorrectAns){
-                console.log("win!")
-                $("#message-display").append("<h3>You Won!</h3");
-                $("#message-display").append('<button id="next">next</button>');
-                $("#next").click(function(){
-                    newGame();
-                })
-
-            }else{
-                console.log("loose!")
-                $("#message-display").append("<h3>You lost. Try another one!</h3");
-                $("#message-display").append('<button id="next">next</button>');
-                $("#next").click(function(){
-                    newGame();
-                })
-            }
+            // condition -------------true ----------------------------------------------false
+            value == textCorrectAns ? $("#message-display").append("<h3>You Won!</h3") : $("#message-display").append("<h3>You lost. Try another one!</h3");
+            count = 0;
+            setTimeout(countDown(), 3000);
         });
 
 
-       
+        // Clock
+        // let count = 30;
+        // $("#seconds").html(count);
+        // function countDown(){
+        //     count--;
+        //     $("#seconds").html(count);
+        // }
+        // setTimeout(countDown, 1000);
 
 
 
-        //Clock
-        let count = 30;
-        $("#seconds").html(count);
+        let count = 10;
         function countDown(){
-            count--;
-            $("#seconds").html(count);
+            let timer = $("#seconds");
+            if(count > 0){
+                count--;
+                timer.html(count);
+                setTimeout(countDown, 1000);
+            }else{
+                nextQuestion();
+            }
         }
-        setTimeout(countDown, 1000);
+        countDown();
+
 
 
         
 
-    });
+    }); // Close THEN.
 
 }); //Close line
